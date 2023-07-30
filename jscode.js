a1df3e54e93d7ca88a6b51cd41231310c44081e0
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-07-31 01:29:10 +0530

    Update jscode.js

--- jscode.js
+++ jscode.js
@@ -1,8 +1,6 @@
 document.querySelector("#clock");
 
 setInterval(function(){
-
 let date= new Date ()
-//console.log(date.toLocaleTimeString());
 clock.innerHTML = date.toLocaleTimeString()
 },1000);
