ba7ea25e348a89754eb1e91b9fbff1dc73b7b595
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-07-31 01:28:10 +0530

    Add files via upload

---
+++ jscode.js
@@ -0,0 +1,8 @@
+document.querySelector("#clock");
+
+setInterval(function(){
+
+let date= new Date ()
+//console.log(date.toLocaleTimeString());
+clock.innerHTML = date.toLocaleTimeString()
+},1000);
